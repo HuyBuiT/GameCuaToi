a5fb98cfa18e0003a35121714928cd9f6580a579
HuyBuiT <100178420+HuyBuiT@users.noreply.github.com>
2022-03-06 11:49:24 +0700

    Gioi thieu

---
+++ GioiThieu.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+string GioiThieu(){
+    string hello="Xin chao moi nguoi da den voi MyGame!";
+    string bio="Day la game giup moi nguoi nang cao kha nang ghi nho cung nhu bo sung kien thuc cho cac be tu 3-6 tuoi!";
+    return hello+bio;
+}
+int main (){
+string s =GioiThieu();
+cout<<s;
+}
